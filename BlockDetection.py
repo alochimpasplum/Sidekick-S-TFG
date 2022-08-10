8340218fb15a5b905bb73c7319e9f526b749cc10
Juan Antonio Lagos <lagoscarrera@gmail.com>
2022-08-10 20:08:24 +0200

    FIX: load yolo repo from local storage

--- BlockDetection.py
+++ BlockDetection.py
@@ -1,5 +1,3 @@
-import copy
-
 from PIL import Image
 from Classes.Block import Block
 from Classes.Text import Text
@@ -11,6 +9,7 @@ import torch
 import json
 import Constants
 import Math_Calcs
+import os
 
 
 def detect_blocks(img: Image) -> [Block]:
@@ -26,7 +25,7 @@ def detect_blocks(img: Image) -> [Block]:
 
 def __get_blocks(image: Image) -> [Block]:
     # Model
-    model = torch.hub.load('ultralytics/yolov5', 'custom', path='./TestStuff/best(L).pt', force_reload=True)
+    model = torch.hub.load(os.path.abspath('./yolov5'), 'custom', source='local', path='./TestStuff/best(L).pt', force_reload=True)
 
     # Inference
     results = model(image)
